f576c376232d6880f4d4e9fa30107bdc6fef41f6
luis augusto <luis.augusto.u@gmail.com>
2023-04-17 18:33:55 -0500

    se instalo tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*',
+    './node_modules/flowbite/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+      require('flowbite/plugin')
+  ],
+}
